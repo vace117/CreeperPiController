1d79ac7621413b1dc95f1e8b0db12e9baaa576a8
val <val@boss.(none)>
2014-01-08 03:44:09 -0600

    COntrol of two servos from a browser achieved

---
+++ AndroidCommandSocket.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+import asynchat, asyncore, socket
+import logging
+from Servos import ServoController
+
+logging.basicConfig(level=logging.DEBUG, format='%(name)s: %(message)s')
+
+ANDROID_HOST = ("192.168.6.1", 4444)
+
+class AndroidSocket(asynchat.async_chat):
+    
+    def __init__(self):
+        self.logger = logging.getLogger("AndroidSocket")
+        
+        # Connect to the Android (when we start the async loop - this is asynchronous)
+        asynchat.async_chat.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect( ANDROID_HOST )
+        
+        # Set up input buffer and define message terminator
+        self.input_buffer = []
+        self.set_terminator("\n")
+        
+        
+    def handle_connect(self):
+        # Init all the servos
+        self.servoController = ServoController(self)
+        
+        self.logger.info("Sending CREEPER_READY status")
+        self.push("CREEPER_READY:\n")
+        
+        
+    def collect_incoming_data(self, data):
+        self.input_buffer.append(data)
+        
+    def found_terminator(self):
+        self.handle_android_command(self.input_buffer[0])
+        self.input_buffer = []
+        
+    def handle_android_command(self, command_data):
+        self.logger.info("Received command: %s" % command_data)
+        self.servoController.process_command(command_data)
+
+        
+
+
+
+try:
+    # Create socket and start the socket event loop 
+    android_socket = AndroidSocket()
+    asyncore.loop()
+finally:
+    print "Cleaning up..."
+    try:
+        android_socket.servoController.stop_all_servos()
+    except AttributeError:
+        pass
+        
---
+++ Servos.py
@@ -0,0 +1,86 @@
+import logging
+import time
+from thread import start_new_thread
+
+import pigpio
+pigpio.start()
+
+class Servo:
+    
+    STEP = 15
+    
+    def __init__(self, name, gpio_pin, min_pos, max_pos, android_socket):
+        self.logger = logging.getLogger("Servo_%s" % gpio_pin)
+        
+        self.android_socket = android_socket
+        self.name = name
+        
+        # GPIO pin to which the servo is connected
+        self.pin = gpio_pin
+        
+        # Pulse widths for the extreme positions of the servo. 
+        # These values are specified in uS 
+        self.min = min_pos
+        self.max = max_pos
+        
+        # Calculated center position in uS
+        self.current_position = self.center_position()
+        
+        # Init the servo pin and move the servo to the center (neutral) position
+        self._pigpio_init_servo()
+    
+    def center_position(self):
+        return self.min + ((self.max - self.min)/2)
+    
+    def _pigpio_init_servo(self):
+        pigpio.set_PWM_frequency(self.pin, 50) # 50Hz pulses
+        pigpio.set_PWM_range(self.pin, 20000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
+        self.move_servo(self.center_position())
+        
+    def move_servo(self, position):
+        pigpio.set_servo_pulsewidth(self.pin, position)
+        self.logger.info("Position = %s" % position)
+        self.android_socket.push("%s:%s\n" % (self.name, position) )
+
+    def decrease_servo_position(self):
+        if ( (self.current_position - self.STEP) >= self.min ):
+            self.current_position -= self.STEP
+            self.move_servo(self.current_position)
+        
+    def increase_servo_position(self):
+        if ( (self.current_position + self.STEP) <= self.max ):
+            self.current_position += self.STEP
+            self.move_servo(self.current_position)
+    
+    def stop_servo(self):
+        self.move_servo(self.center_position())
+        time.sleep(0.5)
+        self.move_servo(0)
+        
+
+class ServoController:
+
+    def __init__(self, android_socket):
+        self.servos = {\
+                       "pan_tilt_azimuth" : Servo("AZIMUTH", 4, 700, 2200, android_socket), \
+                       "pan_tilt_inclination" : Servo("INCLINATION", 17, 700, 2200, android_socket) \
+                       }
+        
+        self.android_socket = android_socket
+        
+    def process_command(self, data):
+        if data == "LOOK_LEFT":
+            start_new_thread( self.servos["pan_tilt_azimuth"].decrease_servo_position, () )
+        elif data == "LOOK_RIGHT":
+            start_new_thread( self.servos["pan_tilt_azimuth"].increase_servo_position, () )
+        elif data == "LOOK_UP":
+            start_new_thread( self.servos["pan_tilt_inclination"].increase_servo_position, () )
+        elif data == "LOOK_DOWN":
+            start_new_thread( self.servos["pan_tilt_inclination"].decrease_servo_position, () )
+            
+    def stop_all_servos(self):
+        for servo in self.servos.values():
+            servo.stop_servo()
+        
+        pigpio.stop()
+    
