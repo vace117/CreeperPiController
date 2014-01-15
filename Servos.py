ed6687bf9c86c67d23c47af96711a246b0a8f299
val <val>
2014-01-15 02:51:53 -0600

    Added LOOK_CENTER command, removed threaded socket writes into main thread, added code to prevent jerky turns

--- Servos.py
+++ Servos.py
@@ -1,4 +1,5 @@
 import logging
+import math
 import time
 from thread import start_new_thread
 
@@ -7,7 +8,8 @@ pigpio.start()
 
 class Servo:
     
-    STEP = 15
+    STEP = 40
+    SLEEP = 0.02
     
     def __init__(self, name, gpio_pin, min_pos, max_pos, android_socket):
         self.logger = logging.getLogger("Servo_%s" % gpio_pin)
@@ -37,24 +39,37 @@ class Servo:
         pigpio.set_PWM_range(self.pin, 20000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
         self.move_servo(self.center_position())
         
-    def move_servo(self, position):
-        pigpio.set_servo_pulsewidth(self.pin, position)
-        self.logger.info("Position = %s" % position)
-        self.android_socket.push("%s:%s\n" % (self.name, position) )
+    def move_servo(self, new_position):
+	old_position = self.current_position
+	if (new_position != 0 and math.fabs(old_position - new_position) > self.STEP):
+	    if old_position < new_position:
+	    	for pos in range(old_position, new_position + self.STEP, self.STEP):
+		   pigpio.set_servo_pulsewidth(self.pin, pos)
+		   time.sleep(self.SLEEP)
+	    if old_position > new_position:
+		for pos in range(old_position, new_position - self.STEP, -self.STEP):
+                   pigpio.set_servo_pulsewidth(self.pin, pos)
+		   time.sleep(self.SLEEP)
+
+        pigpio.set_servo_pulsewidth(self.pin, new_position)
+
+	self.current_position = new_position
+        self.logger.info("Position = %s" % new_position)
+
+    def center_servo(self):
+        self.move_servo(self.center_position())
 
     def decrease_servo_position(self):
         if ( (self.current_position - self.STEP) >= self.min ):
-            self.current_position -= self.STEP
-            self.move_servo(self.current_position)
+            self.move_servo(self.current_position - self.STEP)
         
     def increase_servo_position(self):
         if ( (self.current_position + self.STEP) <= self.max ):
-            self.current_position += self.STEP
-            self.move_servo(self.current_position)
+            self.move_servo(self.current_position + self.STEP)
     
     def stop_servo(self):
-        self.move_servo(self.center_position())
-        time.sleep(0.5)
+        self.center_servo()
+        time.sleep(1)
         self.move_servo(0)
         
 
@@ -71,12 +86,21 @@ class ServoController:
     def process_command(self, data):
         if data == "LOOK_LEFT":
             start_new_thread( self.servos["pan_tilt_azimuth"].decrease_servo_position, () )
+            self.android_socket.push("%s:%s\n" % (self.servos["pan_tilt_azimuth"].name, self.servos["pan_tilt_azimuth"].current_position) )
         elif data == "LOOK_RIGHT":
             start_new_thread( self.servos["pan_tilt_azimuth"].increase_servo_position, () )
+            self.android_socket.push("%s:%s\n" % (self.servos["pan_tilt_azimuth"].name, self.servos["pan_tilt_azimuth"].current_position) )
         elif data == "LOOK_UP":
             start_new_thread( self.servos["pan_tilt_inclination"].increase_servo_position, () )
+            self.android_socket.push("%s:%s\n" % (self.servos["pan_tilt_inclination"].name, self.servos["pan_tilt_inclination"].current_position) )
         elif data == "LOOK_DOWN":
             start_new_thread( self.servos["pan_tilt_inclination"].decrease_servo_position, () )
+            self.android_socket.push("%s:%s\n" % (self.servos["pan_tilt_inclination"].name, self.servos["pan_tilt_inclination"].current_position) )
+        elif data == "LOOK_CENTER":
+	    start_new_thread( self.servos["pan_tilt_azimuth"].center_servo, () )
+	    start_new_thread( self.servos["pan_tilt_inclination"].center_servo, () )
+            self.android_socket.push("%s:%s\n" % (self.servos["pan_tilt_azimuth"].name, self.servos["pan_tilt_azimuth"].current_position) )
+            self.android_socket.push("%s:%s\n" % (self.servos["pan_tilt_inclination"].name, self.servos["pan_tilt_inclination"].current_position) )
             
     def stop_all_servos(self):
         for servo in self.servos.values():
